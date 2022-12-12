6e793544488d6466e27b7be57d4ec2c63b0f6e64
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-12 22:00:34 +0530

    Print prime palindrome numbers in range

--- com/bridgelabz/PrimePalindrome.java
+++ com/bridgelabz/PrimePalindrome.java
@@ -1,26 +1,35 @@
 package com.bridgelabz;
 
-public class PrimePalindrome extends PrimeNumbersInRange{
-
-    public void palindromeNumber(int number){
+/**
+ * @author Govardhan Reddy
+ */
+public class PrimePalindrome extends PrimeNumbersInRange {
+    /**
+     * Given number is palindrome number or not
+     */
+    public void palindromeNumber(int number) {
         int duplicate = number;
-        int remainder=0;
-        int reverse=0;
-        while (number!=0){
-            remainder=number%10;
-            reverse=reverse*10+remainder;
-            number=number/10;
+        int remainder = 0;
+        int reverse = 0;
+        while (number != 0) {
+            remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
         }
-        if (reverse==duplicate){
-            System.out.print(duplicate+" ,");
+        if (reverse == duplicate) {
+            System.out.print(duplicate + " ,");
         }
     }
 
+    /**
+     * Print prime palindrome number in range
+     */
     public static void main(String[] args) {
         PrimePalindrome palindrome = new PrimePalindrome();
-        for (int i = 0;i<=1000;i++) {
+        int firstNumber = 0;//initial number
+        int lastNumber = 1000;//final number
+        for (int i = firstNumber; i <= lastNumber; i++) {
             palindrome.palindromeNumber(palindrome.primeNumber(i));
         }
     }
-
 }
